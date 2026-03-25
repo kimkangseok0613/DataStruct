c673b6b9ef5d41bb20fdf47975d2dfba86b66810
kimkangseok0613 <ks0006131@gmail.com>
2026-03-25 21:41:45 +0900

    (2026.03.25) 우선순위 큐

---
+++ Class 07th (Circle Queue)/Source.cpp
@@ -0,0 +1,83 @@
+﻿#include<iostream>
+
+#define SIZE 4
+
+using namespace std;
+
+template <typename T>
+class Queue
+{
+private:
+	int rear;
+	int front;
+
+	T container[SIZE];
+public:
+	Queue()
+	{
+		rear = SIZE - 1;
+		front = SIZE - 1;
+
+		for (int i = 0; i < SIZE; i++)
+		{
+			container[i] = NULL;
+		}
+	}
+	void push(T data)
+	{
+		if (front == (rear + 1) % SIZE)
+		{
+			cout << "Circle queue overflow" << endl;
+		}
+		else
+		{
+			rear = (rear + 1) % SIZE;
+
+			container[rear] = data;
+		}
+	}
+	void pop()
+	{
+		if (empty())
+		{
+			cout << "Circle queue is empty" << endl;
+		}
+		else
+		{
+			front = (front + 1) % SIZE;
+
+			container[front] = NULL;
+		}
+	}
+	const bool& empty()
+	{
+		return front == rear;
+	}
+	const T& peek()
+	{
+		return container[(front + 1) % SIZE];
+	}
+};
+
+int main()
+{
+	Queue<int> queue;
+
+	queue.push(10);
+	queue.push(20);
+	queue.push(30);
+
+	while (queue.empty() == false)
+	{
+		cout << queue.peek() << endl;
+
+		queue.pop();
+	}
+
+	queue.push(40);
+	queue.push(50);
+	queue.push(60);
+	queue.push(70);
+
+	return 0;
+}
--- Program/Program.cpp
+++ Program/Program.cpp
@@ -1,61 +1,22 @@
 ﻿#include<iostream>
 
-#define SIZE 5
-
 using namespace std;
 
 template <typename T>
-class Queue
+class Priority_queue
 {
 private:
-	int rear;
-	int front;
-	
-	T container[SIZE];
+
 public:
-	Queue()
+	Priority_queue()
 	{
-		rear = 0;
-		front = 0;
-		for (int i = 0; i < SIZE; i++)
-		{
-			container[i] = NULL;
-		}
-	}
-	void push(T data)
-	{
-		if (rear >= SIZE)
-		{
-			cout << "Linear queue overflow" << endl;
-		}
-		else
-		{
-			container[rear++] = data;
-		}
-	}
-	void pop()
-	{
-		while (front == rear)
-		{
-			container[front++] = NULL;
-		}
-	}
-	const bool& empty()
-	{
-		return front == rear;
+
 	}
 };
 
 int main()
 {
-	Queue<int> queue;
-
-	queue.push(10);
-	queue.push(20);
-	queue.push(30);
-	queue.push(40);
-	queue.push(50);
-	queue.push(60);
-
+	Priority_queue<int> priority_queue;
+	
 	return 0;
 }
