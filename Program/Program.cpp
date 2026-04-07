cdfee000e26b34d3708c1741a26622ed7271fb75
kimkangseok0613 <ks0006131@gmail.com>
2026-04-07 20:20:54 +0900

    (2026.04.06) 편향 이진 트리

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -3,141 +3,79 @@
 using namespace std;
 
 template <typename T>
-class Graph
+class Set
 {
 private:
 	struct Node
 	{
 		T data;
-		Node* next;
 
-		Node(T data, Node* link = nullptr)
+		Node* left = nullptr;
+		Node* right = nullptr;
+
+		Node(T data)
 		{
 			this->data = data;
-
-			next = link;
 		}
 	};
-
-	int size;		// 정점의 개수
-	int count;		// 인접 리스트의 크기
-	int capacity;	// 최대 용량
-
-	T* vertex;		// 정점의 집합
-	Node** list;	// 인접 리스트
+	Node* root;
 public:
-	Graph()
+	Set()
 	{
-		size = 0;
-		count = 0;
-		capacity = 0;
-
-		vertex = nullptr;
-		list = nullptr;
+		root = nullptr;
 	}
-	void resize(int newSize)
+	void insert(T data)
 	{
-		capacity = newSize;
-		T* container = new T[capacity];
-
-		for (int i = 0; i < capacity; i++)
+		if (root == nullptr)
 		{
-			container[i] = NULL;
-		}
-		for (int i = 0; i < size; i++)
-		{
-			container[i] = vertex[i];
-		}
-		delete[] vertex;
-
-		vertex = container;
-	}
-	void resize()
-	{
-		Node** newList = new Node * [size];
-
-		for (int i = 0; i < size; i++)
-		{
-			newList[i] = nullptr;
-		}
-		for (int i = 0; i < count; i++)
-		{
-			newList[i] = list[i];
-		}
-		delete[] list;
-
-		list = newList;
-
-		count = size;
-	}
-	void push(T data)
-	{
-		if (capacity <= 0)
-		{
-			resize(1);
-		}
-		else if (size >= capacity)
-		{
-			resize(capacity * 2);
-		}
-		vertex[size++] = data;
-	}
-	void edge(int i, int j)
-	{
-		if (size <= 0)
-		{
-			cout << "adjacency list is empty" << endl;
-		}
-		else if (i >= size || j >= size)
-		{
-			cout << "index out of range" << endl;
+			root = new Node(data);
 		}
 		else
 		{
-			if (list == nullptr)
-			{
-				list = new Node* [size];
+			Node* currentNode = root;
 
-				for (int i = 0; i < size; i++)
-				{
-					list[i] = nullptr;
-				}
-				count = size;
-			}
-			else if (count < size)
+			while (true)
 			{
-				resize();
+				if (data < currentNode->data)
+				{
+					if (currentNode->left == nullptr)
+					{
+						currentNode->left = new Node(data);
+						break;
+					}
+					else
+					{
+						currentNode = currentNode->left;
+					}
+				}
+				else if (data > currentNode->data)
+				{
+					if (currentNode->right == nullptr)
+					{
+						currentNode->right = new Node(data);
+						break;
+					}
+					else
+					{
+						currentNode = currentNode->right;
+					}
+				}
+				else
+				{
+					break;
+				}
 			}
-			list[i] =new Node(vertex[j],list[i]);
-			list[j] =new Node(vertex[i],list[j]);
 		}
-	}
-	void render()
-	{
-		Node* currentNode;
-		//A >> B
-		//B >> C >> A
-		//C
-		//D
-		// 첫번째 인덱스 출력, 2번째 인덱스 출력
 	}
 };
 
 int main()
 {
-	Graph<char> graph;
+	Set<int> set;
 
-	graph.push('A');
-	graph.push('B');
-	graph.push('C');
-	graph.push('D');
-
-	graph.edge(1, 2);
-	graph.edge(1, 3);
-
-	graph.push('E');
-
-	graph.edge(2, 4);
-
+	set.insert(10);
+	set.insert(15);
+	set.insert(6);
+	
 	return 0;
 }
